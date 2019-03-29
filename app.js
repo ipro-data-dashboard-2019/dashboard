76fafef3e41db2d64ef965df80fe0d84c3c691b4
David Delgado <ddelgad5@hawk.iit.edu>
2019-03-28 19:23:58 -0500

    Add basic packages

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+module.exports = app;
