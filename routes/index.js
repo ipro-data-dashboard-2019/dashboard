bc28392aa4e4ee6a97dd9368cda832f7fdc9f146
David Delgado <ddelgad5@hawk.iit.edu>
2019-04-18 20:51:51 -0500

    Add api functionality

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,14 @@ var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('Gonna grab some notifications');
-  request.post({url:'https://ipro-redcross.herokuapp.com/radioman/last/1'}, function optionalCallback(err, httpResponse, body) {
+  request.post({url:'https://ipro-redcross.herokuapp.com/radioman/last/10'}, function optionalCallback(err, httpResponse, body) {
     if (err) {
       res.send(err);
     }
     console.log('We got a response!');
-    console.log(body);
-    //console.log(httpResponse);
+    var noties = JSON.parse(body);
+    res.render('index', { notifications: noties});
   });
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
