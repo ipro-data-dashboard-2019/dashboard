3f45ca1479547c7aa1b15d637483af2dc8c093bd
David Delgado <ddelgad5@hawk.iit.edu>
2019-04-18 19:34:38 -0500

    Test notifications api

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,15 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('Gonna grab some notifications');
+  request.post({url:'https://ipro-redcross.herokuapp.com/radioman/last/1'}, function optionalCallback(err, httpResponse, body) {
+    if (err) {
+      res.send(err);
+    }
+    console.log('We got a response!');
+    console.log(body);
+    //console.log(httpResponse);
+  });
   res.render('index', { title: 'Express' });
 });
 
