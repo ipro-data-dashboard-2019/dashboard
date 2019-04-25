287e10332c9c0f8b06b5f39563f10cbc4c9b1b0d
David Delgado <ddelgad5@hawk.iit.edu>
2019-04-25 17:57:58 -0500

    Add functionality to flag cards

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,15 @@
 var socket = io();
+
+$('#card-list > .card').ready(function() {
+  console.log("Cards have been loaded");
+  $('.card').click(function() {
+    console.log("A card was clicked");
+    console.log(this);
+    let a = $(this);
+    if (!a.hasClass("flagged")){
+      $('#flag-list').append(a.clone());
+      a.addClass("flagged bg-warning").removeClass("bg-light");
+    }
+    else console.log("Card has already been flagged");
+  });
+});
